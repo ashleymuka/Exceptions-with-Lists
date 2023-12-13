3fc3fafa5ccbec339dd73704cb5aed913931019f
ashleymuka <153102801+ashleymuka@users.noreply.github.com>
2023-12-12 22:27:14 -0600

    Update exceptions_with_lists.py

--- exceptions_with_lists.py
+++ exceptions_with_lists.py
@@ -1,24 +1,18 @@
 '''
-Author: Ashley Muka
-Assignment Tilte:Exceptions with lists
-Assignment Description: Write a program that outputs the name specified
-by the list index entered by the user
-Due Date: 09/11/2023
-Date Created: 09/11/2023
-Date Last Modified: 09/11/2023
+Description: Write a program that outputs the name specified by the list index entered by the user
 
 '''
 
-#input
+
 names = ['Ryley', 'Edan', 'Reagan', 'Henry', 'Caius', 'Jane', 'Guto', 'Sonya', 'Tyrese', 'Johnny']
 
 index = int(input())
 
-#process and output
+
 try:
     print(f'Name: {names[index]}')
 
-#process and output
+
 except IndexError as error:
     print("Exception! list index out of range")
 
